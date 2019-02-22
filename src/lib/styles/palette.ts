336d2d1c1070c4b124f01735cbeb808806596cc0
velopert <public.velopert@gmail.com>
2019-02-23 00:14:02 +0900

    Creates palette

---
+++ src/lib/styles/palette.ts
@@ -0,0 +1,26 @@
+const palette = {
+  /* teal */
+  teal0: '#F3FFFB',
+  teal1: '#C3FAE8',
+  teal2: '#96F2D7',
+  teal3: '#63E6BE',
+  teal4: '#38D9A9',
+  teal5: '#20C997',
+  teal6: '#12B886',
+  teal7: '#0CA678',
+  teal8: '#099268',
+  teal9: '#087F5B',
+  /* gray */
+  gray0: '#F8F9FA',
+  gray1: '#F1F3F5',
+  gray2: '#E9ECEF',
+  gray3: '#DEE2E6',
+  gray4: '#CED4DA',
+  gray5: '#ADB5BD',
+  gray6: '#868E96',
+  gray7: '#495057',
+  gray8: '#343A40',
+  gray9: '#212529',
+};
+
+export default palette;
