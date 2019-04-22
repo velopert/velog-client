3bd798098c7bbef796edb3781173db2e1d950191
velopert <public.velopert@gmail.com>
2019-04-22 23:25:57 +0900

    Comments out TMI

--- src/components/write/AskChangeEditor.tsx
+++ src/components/write/AskChangeEditor.tsx
@@ -27,7 +27,7 @@ const AskChangeEditor: React.FC<AskChangeEditorProps> = ({
         onConfirm={onConfirm}
       >
         에디터 모드를 전환하시겠습니까?
-        <br />
+        {/* <br />
         확인을 누르시면 지금까지 입력하신 모든 내용이 마크다운 포맷으료
         변환됩니다.
         <br />
@@ -35,7 +35,7 @@ const AskChangeEditor: React.FC<AskChangeEditorProps> = ({
         <div style={{ fontSize: '0.875rem' }}>
           <b>주의:</b> 변환 후 코드블록의 언어 타입을 수동으로 입력해주셔야
           합니다.
-        </div>
+        </div> */}
       </PopupOKCancel>
     );
   }
