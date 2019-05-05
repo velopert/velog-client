c606bf8f3fcbd494b0783ec05c37b528400787a2
velopert <public.velopert@gmail.com>
2019-05-06 01:32:42 +0900

    Hide act warning in react testing library

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,18 @@
 import 'react-testing-library/cleanup-after-each';
 import 'jest-dom/extend-expect';
+
+// this is just a little hack to silence a warning that we'll get until react
+// fixes this: https://github.com/facebook/react/pull/14853
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args: any[]) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
