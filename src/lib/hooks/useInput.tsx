9d549af616e5a58bc829cd233dd54ede6fe6fa56
velopert <public.velopert@gmail.com>
2019-06-22 23:18:20 +0900

    Supports textarea

--- src/lib/hooks/useInput.tsx
+++ src/lib/hooks/useInput.tsx
@@ -2,8 +2,11 @@ import { useState, useCallback } from 'react';
 
 export default function useInput(defaultValue: string) {
   const [input, setInput] = useState(defaultValue);
-  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  }, []);
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      setInput(e.target.value);
+    },
+    [],
+  );
   return [input, onChange] as [string, typeof onChange];
 }
