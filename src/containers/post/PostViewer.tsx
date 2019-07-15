844ba01a446585ff4c420275991ef084c223c549
velopert <public.velopert@gmail.com>
2019-07-15 23:53:04 +0900

    Convert PostViewer to use apollo hooks

--- src/containers/post/PostViewer.tsx
+++ src/containers/post/PostViewer.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useReducer, Dispatch, useContext } from 'react';
+import React, {
+  createContext,
+  useReducer,
+  Dispatch,
+  useContext,
+  useEffect,
+} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Query, QueryResult } from 'react-apollo';
 import { READ_POST, SinglePost } from '../../lib/graphql/post';
@@ -9,6 +15,7 @@ import { RootState } from '../../modules';
 import { postActions } from '../../modules/post';
 import PostViewerProvider from '../../components/post/PostViewerProvider';
 import { useUserId } from '../../lib/hooks/useUser';
+import { useQuery } from 'react-apollo-hooks';
 
 export interface PostViewerProps {
   username: string;
@@ -19,52 +26,50 @@ const PostViewer: React.FC<PostViewerProps> = ({ username, urlSlug }) => {
   const postId = useSelector((state: RootState) => state.post.id);
   const userId = useUserId();
   const dispatch = useDispatch();
+  const readPost = useQuery<{ post: SinglePost }>(READ_POST, {
+    variables: {
+      username,
+      url_slug: urlSlug,
+    },
+  });
+
+  const { loading, error, data } = readPost;
+
+  useEffect(() => {
+    if (!data) return;
+    if (!data.post) return;
+    dispatch(postActions.setPostId(data.post.id));
+  }, [data, dispatch]);
+
+  if (error) {
+    console.log(error);
+    return null;
+  }
+
+  if (loading) return null;
+  if (!data) return null;
+
+  const { post } = data;
+
   return (
     <PostViewerProvider>
-      <Query
-        query={READ_POST}
-        variables={{
-          username,
-          url_slug: urlSlug,
-        }}
-      >
-        {({ loading, error, data }: QueryResult<{ post: SinglePost }>) => {
-          if (error) {
-            console.log(error);
-            return null; // SHOW ERROR
-          }
-          if (loading) return null; // TODO: show placeholder
-          if (!data || !data.post) return null;
-          const { post } = data;
-
-          if (postId !== post.id) {
-            dispatch(postActions.setPostId(post.id));
-          }
-          return (
-            <>
-              <PostHead
-                title={post.title}
-                tags={post.tags}
-                username={username}
-                date={post.released_at}
-                thumbnail={post.thumbnail}
-                series={post.series}
-                hideThumbnail={
-                  !!post.thumbnail && post.body.includes(post.thumbnail)
-                }
-                postId={post.id}
-                ownPost={post.user.id === userId}
-              />
-              <PostContent isMarkdown={post.is_markdown} body={post.body} />
-              <PostComments
-                count={post.comments_count}
-                comments={post.comments}
-                postId={post.id}
-              />
-            </>
-          );
-        }}
-      </Query>
+      <PostHead
+        title={post.title}
+        tags={post.tags}
+        username={username}
+        date={post.released_at}
+        thumbnail={post.thumbnail}
+        series={post.series}
+        hideThumbnail={!!post.thumbnail && post.body.includes(post.thumbnail)}
+        postId={post.id}
+        ownPost={post.user.id === userId}
+      />
+      <PostContent isMarkdown={post.is_markdown} body={post.body} />
+      <PostComments
+        count={post.comments_count}
+        comments={post.comments}
+        postId={post.id}
+      />
     </PostViewerProvider>
   );
 };
--- src/containers/post/__tests__/PostViewer.test.tsx
+++ src/containers/post/__tests__/PostViewer.test.tsx
@@ -17,202 +17,110 @@ function createClient(mocks: MockedResponse[]) {
   });
 }
 
+const samplePost = {
+  id: '6533da20-b351-11e8-9696-f1fffe8a36f1',
+  title: '제목입니다',
+  released_at: '2018-09-08T10:24:40.642Z',
+  updated_at: '2019-01-28T13:51:10.999Z',
+  tags: ['react', 'redux'],
+  body: '내용입니다',
+  is_markdown: true,
+  is_private: false,
+  is_temp: false,
+  thumbnail:
+    'https://images.velog.io/post-images/velopert/654650b0-b351-11e8-9696-f1fffe8a36f1/redux.png',
+  user: {
+    id: 'c76ccc50-b34d-11e8-b01f-598f1220d1c8',
+    username: 'velopert',
+    profile: {
+      display_name: 'Minjun Kim',
+      thumbnail:
+        'https://images.velog.io/profiles/velopert/thumbnails/1536400727.98.png',
+      short_bio: 'velopert@Laftel Inc. 재미있는것만 골라서 하는 개발자입니다.',
+    },
+    velog_config: {
+      id: '5ad9f60b-4b95-42e9-a32c-eae222c79bf1',
+      title: 'VELOPERT.LOG',
+    },
+  },
+  comments_count: 2,
+  comments: [
+    {
+      id: '70d24d3b-a6ce-46a3-86c5-1cba95843841',
+      user: {
+        id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
+        username: 'blablabla',
+        profile: {
+          thumbnail: null,
+        },
+      },
+      text: 'Hey there',
+      replies_count: 0,
+      created_at: '2019-06-13T14:27:25.463Z',
+      level: 0,
+      deleted: false,
+    },
+    {
+      id: '0f700ebd-0dec-469a-adb8-c47bae2b8f18',
+      user: {
+        id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
+        username: 'blablabla',
+        profile: {
+          thumbnail: null,
+        },
+      },
+      text: 'Hey there',
+      replies_count: 0,
+      created_at: '2019-06-13T14:27:31.265Z',
+      level: 0,
+      deleted: false,
+    },
+  ],
+  series: null,
+};
+
 describe('PostViewer', () => {
   const setup = (props: Partial<PostViewerProps> = {}, overrideMocks?: any) => {
     const initialProps: PostViewerProps = {
       username: 'velopert',
       urlSlug: 'sample',
     };
-    const mocks = [
-      {
-        request: {
-          query: READ_POST,
-          variables: {
-            username: 'velopert',
-            url_slug: 'sample',
-          },
-        },
-        result: {
-          data: {
-            post: {
-              id: '6533da20-b351-11e8-9696-f1fffe8a36f1',
-              title: '제목입니다',
-              released_at: '2018-09-08T10:24:40.642Z',
-              updated_at: '2019-01-28T13:51:10.999Z',
-              tags: ['react', 'redux'],
-              body: '내용입니다',
-              is_markdown: true,
-              is_private: false,
-              is_temp: false,
-              thumbnail:
-                'https://images.velog.io/post-images/velopert/654650b0-b351-11e8-9696-f1fffe8a36f1/redux.png',
-              user: {
-                id: 'c76ccc50-b34d-11e8-b01f-598f1220d1c8',
-                username: 'velopert',
-                profile: {
-                  display_name: 'Minjun Kim',
-                  thumbnail:
-                    'https://images.velog.io/profiles/velopert/thumbnails/1536400727.98.png',
-                  short_bio:
-                    'velopert@Laftel Inc. 재미있는것만 골라서 하는 개발자입니다.',
-                },
-                velog_config: {
-                  id: '5ad9f60b-4b95-42e9-a32c-eae222c79bf1',
-                  title: 'VELOPERT.LOG',
-                },
-              },
-              comments_count: 10,
-              comments: [
-                {
-                  id: '70d24d3b-a6ce-46a3-86c5-1cba95843841',
-                  user: {
-                    id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
-                    username: 'blablabla',
-                    profile: {
-                      thumbnail: null,
-                    },
-                  },
-                  text: 'Hey there',
-                  replies_count: 0,
-                  created_at: '2019-06-13T14:27:25.463Z',
-                  level: 0,
-                  deleted: false,
-                },
-                {
-                  id: '0f700ebd-0dec-469a-adb8-c47bae2b8f18',
-                  user: {
-                    id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
-                    username: 'blablabla',
-                    profile: {
-                      thumbnail: null,
-                    },
-                  },
-                  text: 'Hey there',
-                  replies_count: 0,
-                  created_at: '2019-06-13T14:27:31.265Z',
-                  level: 0,
-                  deleted: false,
-                },
-                {
-                  id: '90f3b558-4af3-41bb-95dd-ed9571609f25',
-                  user: {
-                    id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
-                    username: 'blablabla',
-                    profile: {
-                      thumbnail: null,
-                    },
-                  },
-                  text: 'Hey there',
-                  replies_count: 0,
-                  created_at: '2019-06-13T14:27:53.898Z',
-                  level: 0,
-                  deleted: false,
-                },
-                {
-                  id: 'd4365762-08e8-4928-a387-4255e4392291',
-                  user: {
-                    id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
-                    username: 'blablabla',
-                    profile: {
-                      thumbnail: null,
-                    },
-                  },
-                  text: 'Hey there',
-                  replies_count: 3,
-                  created_at: '2019-06-13T14:42:28.873Z',
-                  level: 0,
-                  deleted: false,
-                },
-                {
-                  id: '383d590d-d3ed-4f07-994e-cbea3127195d',
-                  user: {
-                    id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
-                    username: 'blablabla',
-                    profile: {
-                      thumbnail: null,
-                    },
-                  },
-                  text: 'Hey there',
-                  replies_count: 0,
-                  created_at: '2019-06-13T14:42:58.100Z',
-                  level: 0,
-                  deleted: false,
-                },
-                {
-                  id: 'a0bc6974-582a-4eb1-8273-627e1df7c527',
-                  user: {
-                    id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
-                    username: 'blablabla',
-                    profile: {
-                      thumbnail: null,
-                    },
-                  },
-                  text: 'aw3b5aw35w35bw3a',
-                  replies_count: 0,
-                  created_at: '2019-06-14T14:43:22.312Z',
-                  level: 0,
-                  deleted: false,
-                },
-                {
-                  id: '86fa0b3f-1493-424c-aff4-49e1b059caf6',
-                  user: {
-                    id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
-                    username: 'blablabla',
-                    profile: {
-                      thumbnail: null,
-                    },
-                  },
-                  text: '잘된거니?',
-                  replies_count: 0,
-                  created_at: '2019-06-14T14:43:27.840Z',
-                  level: 0,
-                  deleted: false,
-                },
-                {
-                  id: '4e76da59-048a-4d2d-b11f-4f6194dff4f5',
-                  user: {
-                    id: '0bcdf3e5-a228-42c3-8b52-3f0dc118dfd8',
-                    username: 'blablabla',
-                    profile: {
-                      thumbnail: null,
-                    },
-                  },
-                  text: 'afasdfsdf',
-                  replies_count: 0,
-                  created_at: '2019-06-14T14:49:08.871Z',
-                  level: 0,
-                  deleted: false,
-                },
-              ],
-              series: null,
-            },
-          },
-        },
-      },
-    ];
 
-    const client = createClient([]);
+    const client = createClient(overrideMocks || []);
 
     const utils = renderWithRedux(
-      <MockedProvider mocks={overrideMocks || mocks} addTypename={false}>
-        <ApolloProvider client={client}>
-          <MemoryRouter>
-            <PostViewer {...initialProps} {...props} />
-          </MemoryRouter>
-        </ApolloProvider>
-      </MockedProvider>,
+      <ApolloProvider client={client}>
+        <MemoryRouter>
+          <PostViewer {...initialProps} {...props} />
+        </MemoryRouter>
+      </ApolloProvider>,
     );
     return {
       ...utils,
     };
   };
   it('renders post correctly', async () => {
-    const { getByText, getByAltText } = setup();
+    const { getByText, getByAltText } = setup({}, [
+      {
+        request: {
+          query: READ_POST,
+          variables: {
+            username: 'velopert',
+            url_slug: 'sample',
+          },
+        },
+        result: {
+          data: {
+            post: samplePost,
+          },
+        },
+      },
+    ]);
     await waitForElement(() => getByText('제목입니다'));
     waitForElement(() => getByAltText('post-thumbnail'));
   });
   it('hides thumbnail if exists in body', async () => {
-    const mocks = [
+    const { getByText, queryByAltText } = setup({}, [
       {
         request: {
           query: READ_POST,
@@ -224,42 +132,14 @@ describe('PostViewer', () => {
         result: {
           data: {
             post: {
-              id: '6533da20-b351-11e8-9696-f1fffe8a36f1',
-              title: '제목입니다',
-              released_at: '2018-09-08T10:24:40.642Z',
-              updated_at: '2019-01-28T13:51:10.999Z',
-              tags: ['react', 'redux'],
+              ...samplePost,
               body:
                 '내용입니다 ![](https://images.velog.io/post-images/velopert/654650b0-b351-11e8-9696-f1fffe8a36f1/redux.png)',
-              is_markdown: true,
-              is_private: false,
-              is_temp: false,
-              thumbnail:
-                'https://images.velog.io/post-images/velopert/654650b0-b351-11e8-9696-f1fffe8a36f1/redux.png',
-              user: {
-                id: 'c76ccc50-b34d-11e8-b01f-598f1220d1c8',
-                username: 'velopert',
-                profile: {
-                  display_name: 'Minjun Kim',
-                  thumbnail:
-                    'https://images.velog.io/profiles/velopert/thumbnails/1536400727.98.png',
-                  short_bio:
-                    'velopert@Laftel Inc. 재미있는것만 골라서 하는 개발자입니다.',
-                },
-                velog_config: {
-                  id: '5ad9f60b-4b95-42e9-a32c-eae222c79bf1',
-                  title: 'VELOPERT.LOG',
-                },
-              },
-              comments: [],
-              comments_count: 0,
-              series: null,
             },
           },
         },
       },
-    ];
-    const { getByText, queryByAltText } = setup(undefined, mocks);
+    ]);
     await waitForElement(() => getByText('제목입니다'));
     const thumbnail = queryByAltText('post-thumbnail');
     expect(thumbnail).toBeFalsy();
