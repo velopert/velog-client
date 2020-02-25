30e0200efad2b7fa55074c74aa31c5d9483a6fd7
velopert <public.velopert@gmail.com>
2020-02-26 00:32:18 +0900

    Resolve issue #74
    Set fixed height to PublishSeriesList

--- src/containers/write/PublishSeriesList.tsx
+++ src/containers/write/PublishSeriesList.tsx
@@ -2,7 +2,8 @@ import SelectableList from '../../components/common/SelectableList';
 import styled from 'styled-components';
 
 const PublishSeriesList = styled(SelectableList)`
-  flex: 1;
+  height: 16.5625rem;
+  overflow-y: auto;
 `;
 
 export default PublishSeriesList;
