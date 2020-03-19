987e67bc400eaab8500ad9c8db761c2dcf454ddf
velopert <public.velopert@gmail.com>
2020-03-20 01:32:27 +0900

    Use cloudflare

--- src/lib/optimizeImage.ts
+++ src/lib/optimizeImage.ts
@@ -1,4 +1,9 @@
-export default function optimizeImage(url: string, width: number) {
+export default function optimizeImage(url: string, width?: number) {
   if (!url.includes('https://images.velog.io')) return url;
-  return url.replace('://images', '://img').concat(`?w=${width}`);
+
+  let replaced = url.replace('://images.velog.io', '://media.vlpt.us');
+  if (!width) {
+    return replaced;
+  }
+  return replaced.concat(`?w=${width}`);
 }
