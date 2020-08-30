4a8a3fb362f30e0ae655bc81646674a47375c17a
velopert <public.velopert@gmail.com>
2020-08-30 22:58:59 +0900

    Temporarily disable cloudflare

--- src/lib/optimizeImage.ts
+++ src/lib/optimizeImage.ts
@@ -1,7 +1,9 @@
 export default function optimizeImage(url: string, width?: number) {
   if (!url.includes('https://images.velog.io')) return url;
 
-  let replaced = url.replace('://images.velog.io', '://media.vlpt.us');
+  // let replaced = url.replace('://images.velog.io', '://media.vlpt.us');
+  let replaced = url.replace('://images', '://img');
+
   if (!width) {
     return replaced;
   }
