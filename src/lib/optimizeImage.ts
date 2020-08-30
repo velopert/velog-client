9ac637c6c2bc55075a59b9091eedef01615ee42e
velopert <public.velopert@gmail.com>
2020-08-31 06:57:21 +0900

    Reuse cloudflare since it has been fixed

--- src/lib/optimizeImage.ts
+++ src/lib/optimizeImage.ts
@@ -1,8 +1,8 @@
 export default function optimizeImage(url: string, width?: number) {
   if (!url.includes('https://images.velog.io')) return url;
 
-  // let replaced = url.replace('://images.velog.io', '://media.vlpt.us');
-  let replaced = url.replace('://images', '://img');
+  let replaced = url.replace('://images.velog.io', '://media.vlpt.us'); // Cloudflare
+  // let replaced = url.replace('://images', '://img'); // Cloudfront
 
   if (!width) {
     return replaced;
