dd9795175432ef224a049439283b976b38188020
velopert <public.velopert@gmail.com>
2020-11-01 13:53:25 +0900

    Do not optimize svg

--- src/lib/optimizeImage.ts
+++ src/lib/optimizeImage.ts
@@ -1,5 +1,6 @@
 export default function optimizeImage(url: string, width?: number) {
   if (!url.includes('https://images.velog.io')) return url;
+  if (url.includes('.svg')) return url;
 
   let replaced = url.replace('://images.velog.io', '://media.vlpt.us'); // Cloudflare
   // let replaced = url.replace('://images', '://img'); // Cloudfront
