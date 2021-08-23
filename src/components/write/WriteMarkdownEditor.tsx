de8a2e33b308867341db6a71bff3db7a5a3f9796
bigsaigon333 <likepeppermint@gmail.com>
2021-08-23 20:45:13 +0900

    feat: 마크다운편집기에서 'Ctrl-B' 로 볼드 처리를 할 수 있다

--- src/components/write/WriteMarkdownEditor.tsx
+++ src/components/write/WriteMarkdownEditor.tsx
@@ -196,6 +196,24 @@ export default class WriteMarkdownEditor extends React.Component<
         e.preventDefault();
       }
     }) as any);
+
+    this.codemirror.addKeyMap({
+      // bold
+      'Ctrl-B': (cm) => {
+        const isSurroundedWith = (char: string, str: string) =>
+          str.startsWith(char) && str.endsWith(char);
+
+        const surround = (char: string, str: string) => char + str + char;
+
+        const remove = (char: string, str: string) =>
+          str.slice(char.length, -char.length);
+
+        const toggle = (char: string, str: string) =>
+          isSurroundedWith(char, str) ? remove(char, str) : surround(char, str);
+
+        cm.replaceSelection(toggle('**', cm.getSelection()), 'around');
+      },
+    });
   };
 
   stickToBottomIfNeeded = () => {
