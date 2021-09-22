16fbd2f94c375a9a230d19556e273ac12bee04f2
velopert <public.velopert@gmail.com>
2021-09-23 00:26:36 +0900

    Change process env

--- src/server/CacheManager.ts
+++ src/server/CacheManager.ts
@@ -2,7 +2,7 @@ import Redis from 'ioredis';
 import checkCacheRule from './checkCacheRule';
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
+  host: process.env.REACT_APP_REDIS_HOST || 'localhost',
 });
 
 function createCacheKey(url: string) {
