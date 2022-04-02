ebf08402906ca4667c3609cc7d056f3ce237bd19
velopert <public.velopert@gmail.com>
2022-04-02 15:14:06 +0900

    fix: optimize image for media.vlpt.us

--- src/lib/optimizeImage.ts
+++ src/lib/optimizeImage.ts
@@ -1,5 +1,11 @@
 export default function optimizeImage(url: string, width?: number) {
-  if (!url.includes('https://images.velog.io')) return url;
+  if (
+    !url.includes('https://images.velog.io') &&
+    !url.includes('https://media.vlpt.us')
+  )
+    return url;
+
+  if (url.includes('?')) return url;
   if (url.includes('.svg')) return url;
 
   let replaced = url.replace('://images.velog.io', '://media.vlpt.us'); // Cloudflare
