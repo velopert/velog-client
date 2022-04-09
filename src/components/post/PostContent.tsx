0dea70ea913f39c60fef95db89c784f180385cc8
velopert <public.velopert@gmail.com>
2022-04-10 07:10:42 +0900

    feat: handle encode issue for images inside post

--- src/components/post/PostContent.tsx
+++ src/components/post/PostContent.tsx
@@ -29,10 +29,19 @@ function optimizeImagesFromPost(markdown: string) {
     /(?:!\[(.*?)\]\(https:\/\/images.velog.io\/(.*?)\))/g,
   );
   if (!matches) return markdown;
-  const replacers = matches.map((match) => [
-    match,
-    match.replace('https://images.velog.io', 'https://velog.velcdn.com'),
-  ]);
+  console.log(matches);
+  const replacers = matches.map((match) => {
+    const filename =
+      match.match(/https:\/\/images.velog.io\/(.*?)\)/)?.[1] ?? '';
+    console.log(filename);
+    const proeperlyEncoded = encodeURIComponent(decodeURI(filename));
+    return [
+      match,
+      match
+        .replace('https://images.velog.io', 'https://velog.velcdn.com')
+        .replace(filename, proeperlyEncoded),
+    ];
+  });
   return replacers.reduce((acc, [original, optimized]) => {
     return acc.replace(original, optimized);
   }, markdown);
