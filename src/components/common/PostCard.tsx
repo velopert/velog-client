e3436330209735a8720cdf57bdf6d56ee1d3f721
yeynii <yeynii@gmail.com>
2022-06-21 14:25:09 +0900

    Update src/components/common/PostCard.tsx

--- src/components/common/PostCard.tsx
+++ src/components/common/PostCard.tsx
@@ -53,7 +53,7 @@ function PostCard({ post, forHome, forPost, index }: PostCardProps) {
             widthRatio={1.916}
             heightRatio={1}
             src={optimizeImage(post.thumbnail, 640)}
-            art={`thumbnail of ${getNumberWithOrdinal(index)} post`}
+            art={`thumbnail of ${getNumberWithOrdinal(index + 1)} post`}
           />
         </StyledLink>
       )}
