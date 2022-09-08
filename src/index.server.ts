33ea1762715fca786dca2d46a447891bb6fc1271
velopert <public.velopert@gmail.com>
2022-09-08 20:16:13 +0900

    removes ads.txt for now

--- src/index.server.ts
+++ src/index.server.ts
@@ -22,9 +22,9 @@ app.use(router.routes()).use(router.allowedMethods());
 
 app.use(ssrMiddleware);
 app.use(proxyMiddleware);
-router.get('/ads.txt', (ctx) => {
-  ctx.body = `google.com, pub-5574866530496701, DIRECT, f08c47fec0942fa0`;
-});
+// router.get('/ads.txt', (ctx) => {
+//   ctx.body = `google.com, pub-5574866530496701, DIRECT, f08c47fec0942fa0`;
+// });
 // router.post('/graphql', proxyMiddleware);
 
 app.listen(3001, () => {
