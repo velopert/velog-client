be404b2919dccf8ef8e5fbf1ffa713daf277936a
carrick <carrick@chafgames.com>
2023-08-23 18:32:31 +0900

    feat: add redirect to V3 client host in main page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -12,6 +12,9 @@ import FloatingHeader from '../../components/base/FloatingHeader';
 export type HomePageProps = {};
 
 function HomePage(props: HomePageProps) {
+  if (process.env.NODE_ENV === 'production') {
+    window.location.href = process.env.REACT_APP_CLIENT_V3_HOST!;
+  }
   return (
     <MainTemplate>
       <Header />
