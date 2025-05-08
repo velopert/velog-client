7ce29ae1f9ce860d5b9d6c59aa5c5ecce7c470e8
velopert <public.velopert@gmail.com>
2025-05-08 12:09:10 +0900

    fix: index.server.ts

--- src/index.server.ts
+++ src/index.server.ts
@@ -25,7 +25,7 @@ app.use(router.routes()).use(router.allowedMethods());
 app.use(ssrMiddleware);
 app.use(proxyMiddleware);
 router.get('/ads.txt', (ctx) => {
-  ctx.body = `google.com, pub-9161852896103498, DIRECT, f08c47fec0942fa0`;
+  ctx.body = `google.com, pub-5574866530496701, DIRECT, f08c47fec0942fa0`;
 });
 // router.post('/graphql', proxyMiddleware);
 
